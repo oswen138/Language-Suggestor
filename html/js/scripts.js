04a10cac5094f7be13d1542393b99ba06286f9c2
ophelia swen <ophelia.swen@gmail.com>
2020-08-14 14:14:37 -0700

    add image and text as answer when form is submitted

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -12,11 +12,11 @@ $(document).ready(function() {
 
     if (donut === 'd1' && system === 'sys1' && syntax === 'c1' && library ==='l1' && speed === 's1' && error === 'e1' && color === 'red') {
       $('#answer').text("Python");
-      $('#picture').attr('src', "https://i.insider.com/5ab53db4095b111a068b45b6?width=1100&format=jpeg&auto=webp");
+      $("#img1").show();
 
     } else {
       $('#answer').text("Ruby");
-      document.getElementById('#python');
+      $("#img1").show();;
     }
 
     event.preventDefault();
