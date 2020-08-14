e250e708bfc6c6d0a40781ee9486bafa2c706414
ophelia swen <ophelia.swen@gmail.com>
2020-08-14 15:19:17 -0700

    reconfigure formulas

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -10,13 +10,23 @@ $(document).ready(function() {
     const error = parseInt($("#error").val());
     const color = parseInt($("input:radio[name=color]:checked").val());
 
-    if (donut === 'd1' && system === 'sys1' && syntax === 'c1' && library ==='l1' && speed === 's1' && error === 'e1' && color === 'red') {
-      $('#answer').text("Python");
-      $("#img1").show();
+    if (donut === d3)
 
-    } else {
-      $('#answer').text("Ruby");
+
+
+
+
+
+
+    if (donut == "d1" + system == "sys1" && syntax == "c1" && library == "l1" && speed == "s1" && error === "e" && color == "blue") {
+      $('#answer').text("Python");
       $("#img1").show();;
+    } else if (donut == 'd2' + system == 'sys2' + syntax == 'c2' + library ==='l2' + speed === 's2' + error === 'e2' + color === 'blue') {
+      $('#answer').text("Python");
+      $("#img3").show();;
+    } else {
+    $('#answer').text("animal");
+    $("#img2").show();;
     }
 
     event.preventDefault();
