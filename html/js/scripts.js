0556b892221556a36e990335902d144e689d583f
ophelia swen <ophelia.swen@gmail.com>
2020-08-14 16:57:39 -0700

    attempt to show else statement

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,28 +1,29 @@
 //function to calculate the result of the survey form
 $(document).ready(function() {
   $("form").submit(function(event) {
+    event.preventDefault();
 
-    const donut = parseInt($("donut").val());
-    const system = parseInt($("#system").val());
-    const syntax = parseInt($("#syntax").val());
-    const library = parseInt($("#library").val());
-    const speed = parseInt($("#speed").val());
-    const error = parseInt($("#error").val());
+    const donut = parseInt($("input#donut").val());
+    const system = parseInt($("input#system").val());
+    const syntax = parseInt($("input#syntax").val());
+    const library = parseInt($("input#library").val());
+    const speed = parseInt($("input#speed").val());
+    const error = parseInt($("input#error").val());
     const color = parseInt($("input:radio[name=color]:checked").val());
 
     if (donut === "1" + system === "1" + syntax === "1" + library === "1" + speed === "1" + error === "1" + color === "blue" <=1) {
       $('#answer').text("Go");
       $("#img3").show();
       }
-      else if (donut === "2" & system === "2" + syntax === "2" + library === "2" + speed === "2" + error === "2" + color === "orange") {
+      else if (donut === "2" & system === "2" + syntax === "2" + library === "2" + speed === "2" + error === "2" + color === "red") {
       $('#answer').text("Ruby");
       $("#img2").show();
       }
-      else if (donut === "3" + system === "3" + syntax === "3" + library === "3" + speed === "3" + error === "3" + color === "blue") {
+      else if (donut === "3" + system === "3" + syntax === "3" + library === "3" + speed === "3" + error === "3" + color === "orange") {
         $('#answer').text("Python");
         $("#img2").show();
       }
-      else if (donut === "3" + system === "3" + syntax === "3" + library === "3" + speed === "3" + error === "3" + color === "blue") {
+      else if (donut === "3" || system === "2" || syntax === "1" + library === "1"|| speed === "2" || error === "3" || color === "red") {
         $('#answer').text("Swift");
         $("#img4").show();
       }
@@ -30,7 +31,6 @@ $(document).ready(function() {
       $('#answer').text("none suit you");
       $("#img5").show();
       }
-    event.preventDefault();
   });
 });
 
