749554ca292316c1a7806f54c2000d11c06015f2
ophelia swen <ophelia.swen@gmail.com>
2020-08-14 16:05:39 -0700

    make the first if statement work

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -2,23 +2,25 @@
 $(document).ready(function() {
   $("form").submit(function () {
 
-    const donut = parseInt($("#donut").val());
+    const donut = parseInt($("donut").val());
     const system = parseInt($("#system").val());
     const syntax = parseInt($("#syntax").val());
     const library = parseInt($("#library").val());
     const speed = parseInt($("#speed").val());
-    const error = parseInt($("#error").val());
+    const errpr = parseInt($("#error").val());
     const color = parseInt($("input:radio[name=color]:checked").val());
 
-    if (donut === d3, system === "sys1" , syntax === "c1", library === "l1", speed === "s1", error === "e1", color === "red") {
+    if (donut === "1" + system === "1" + syntax === "1" + library === "1" + speed === "1" + error === "1" + color === "blue" <=1) {
       $('#answer').text("Python");
       $("#img1").show();
     }
-    else {
-    $('#answer').text("animal");
-    $("#img2").show();;
-    }
-
+      else if (donut === "2" + system === "2" + syntax === "2" + library === "2" + speed === "2" + error === "2" + color === "blue" >=3) {
+        $('#answer').text("animal");
+        $("#img2").show();
+      }
+      else {
+        $('#answer').text("none suit you");
+      }
     event.preventDefault();
   });
 });
