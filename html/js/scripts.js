9aab79777317969eb03ed79e2b73e308973a127b
ophelia swen <ophelia.swen@gmail.com>
2020-08-19 19:25:24 -0700

    fix formulas on javascript

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,36 +1,32 @@
 //function to calculate the result of the survey form
 $(document).ready(function() {
-  $("form").submit(function(event) {
+    $("form#form").submit(function(event) {
     event.preventDefault();
 
-    const donut = parseInt($("input#donut").val());
-    const system = parseInt($("input#system").val());
-    const syntax = parseInt($("input#syntax").val());
-    const library = parseInt($("input#library").val());
-    const speed = parseInt($("input#speed").val());
-    const error = parseInt($("input#error").val());
-    const color = parseInt($("input:radio[name=color]:checked").val());
+    const form1 = parseInt($("select#form1").val());
+    const form2 = parseInt($("select#form2").val());
+    const form3 = parseInt($("select#form3").val());
+    const form4 = parseInt($("select#form4").val());
+    const form5 = parseInt($("select#form5").val());
+    const form6 = parseInt($("select#form6").val());
+    const form7 = parseInt($("input:radio[name=color]:checked").val());
 
-    if (donut === "1" + system === "1" + syntax === "1" + library === "1" + speed === "1" + error === "1" + color === "blue" <=1) {
-      $('#answer').text("Go");
+    if ((form1 === 1) && (form2 === 1) && (form3 === 1) && (form4 === 1) && (form5 === 1) && (form6 === 1)) {
       $("#img3").show();
-      }
-      else if (donut === "2" & system === "2" + syntax === "2" + library === "2" + speed === "2" + error === "2" + color === "red") {
-      $('#answer').text("Ruby");
+    } else if ((form1 === 2) && (form2 === 2) && (form3 === 2) && (form4 === 2) && (form5 === 2) && (form6 === 2)) {
       $("#img2").show();
-      }
-      else if (donut === "3" + system === "3" + syntax === "3" + library === "3" + speed === "3" + error === "3" + color === "orange") {
-        $('#answer').text("Python");
-        $("#img2").show();
-      }
-      else if (donut === "3" || system === "2" || syntax === "1" + library === "1"|| speed === "2" || error === "3" || color === "red") {
-        $('#answer').text("Swift");
-        $("#img4").show();
-      }
-      else {
-      $('#answer').text("none suit you");
+    } else if ((form1 === 3) && (form2 === 3) && (form3 === 3) && (form4 === 3 && (form5 === 3) && (form6 === 3)) ){
+      $("#img4").show();
+    } else if ((form1 === 3) && (form2 === 1) && (form3 === 1) && (form4 === 2 && (form5 === 3) && (form6 === 2)) ){
+      $("#img1").show();
+    } else if ((form1 === 1) && (form2 === 2) && (form3 === 3) && (form4 === 1 && (form5 === 2) && (form6 === 3)) ){
+      $("#img1").show();
+    } else {
       $("#img5").show();
-      }
+    }
+
   });
 });
 
+
+   
